7bef9b007eb6d9652647fa94c5152b76cfb78c54
Bill Yeh <billwyeh@yahoo.com>
2013-05-31 11:06:12 -0700

    fixed styles and patent map

--- stats.js
+++ stats.js
@@ -10,10 +10,18 @@ var svg = d3.select(".container").append("svg")
 d3.json("us.json", renderMap);
 
 function renderMap(error, topology) {
-  svg.selectAll("path")
-      .data(topojson.feature(topology, topology.objects.counties).features)
-    .enter().append("path")
-      .attr("d", path);
+  svg.append("path")
+      .datum(topojson.feature(topology, topology.objects.land))
+      .attr("d", path)
+
+  svg.append("path")
+      .datum(topojson.mesh(topology, topology.objects.counties, function(a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); }))
+      .attr("d", path)
+      .attr("class", "county-boundary");
+
+  svg.append("path")
+      .datum(topojson.mesh(topology, topology.objects.states, function(a, b) { return a !== b; }))
+      .attr("d", path)
 }
 
 d3.csv("patents_2010.csv", renderPatents);
@@ -23,5 +31,7 @@ function renderPatents(error, d) {
   svg.selectAll("circle")
       .data(d)
     .enter().append("circle")
-      .attr("transform", function(d) {return "translate(" + projection([d.long,d.lat]) + ")";});
+      .attr("r", 2)
+      .style("fill", "rgba(0, 0, 255, .04")
+      .attr("transform", function(d) {return "translate(" + projection([d.long, d.lat]) + ")";});
 }
