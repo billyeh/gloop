3a86c18892cb8ed420bdee4cac0a7dc7daec69df
Bill Yeh <billwyeh@yahoo.com>
2013-06-01 16:22:41 -0700

    adding data files

---
+++ patent_data/patent_locator.py
@@ -0,0 +1,60 @@
+import sqlite3, json
+
+full = sqlite3.connect("/home/billyeh/workspace/patentwork/2013_data/full_disambiguation.sqlite3")
+patdesc = sqlite3.connect("/home/billyeh/workspace/patentwork/2013_data/patdesc.sqlite3")
+citations = sqlite3.connect("/home/billyeh/workspace/patentwork/2013_data/citations_and_refs_mar262013.sqlite3")
+
+top_patents = [7663607,
+               7674650,
+               7665646,
+               7680184,
+               7732819,
+               7657297,
+               7654956,
+               7665051,
+               7711402,
+               7770144]
+
+patents = []
+points = {}
+points["type"] = "FeatureCollection"
+points["features"] = []
+link = "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&u=%2Fnetahtml%2FPTO%2Fsearch-adv.htm&r=1&p=1&f=G&l=50&d=PTXT&S1=7663607.PN.&OS=pn/"
+
+for top_patent in [7663607]: # top_patents:
+  current = {}
+  current["patent"] = top_patent
+  row = patdesc.execute("SELECT Title FROM patdesc WHERE patent=?", ['0' + str(top_patent)]).fetchone()
+  current["title"] = row[0]
+  row = full.execute("SELECT Longitude, Latitude FROM invpat WHERE patent=?", ['0' + str(top_patent)]).fetchone()
+  current["location"] = [row[0], row[1]]
+  current["link"] = link + str(top_patent)
+
+  current["collection"] = {}
+  current["collection"]["type"] = "FeatureCollection"
+  current["collection"]["features"] = []
+
+  point = {}
+  point["type"] = "Feature"
+  point["geometry"] = {}
+  point["geometry"]["type"] = "Point"
+  point["geometry"]["coordinates"] = current["location"]
+  point["properties"] = {}
+  point["properties"]["color"] = "#FFD300"
+
+  for cite in [7711402]: # citations
+    feature = {}
+    feature["type"] = "Feature"
+    feature["geometry"] = {}
+    feature["geometry"]["type"] = "LineString"
+    feature["geometry"]["coordinates"] = [[-75.308372,39.856213], current["location"]] # [[long, lat], current["location"]]
+    feature["properties"] = {}
+    feature["properties"]["citation"] = cite
+    current["collection"]["features"].append(feature)
+
+  point["radius"] = round(141 / 20) # no. cites / 20
+  points["features"].append(point)
+  patents.append(current)
+
+print(json.dumps(patents, indent=2, separators=(',', ': ')))
+print(json.dumps(points, indent=2, separators=(',', ': ')))
