78cfeca3b75f45c6330ed50db04be4aaafadd360
Bill Yeh <billwyeh@yahoo.com>
2013-07-05 22:25:20 -0700

    add test for points.json

--- patent_data/patent_locator_test.py
+++ patent_data/patent_locator_test.py
@@ -1,6 +1,8 @@
 import json, GeoJSONTest
 
-collections = [c["collection"] for c in json.load(open('patents.json'))]
-for c in collections:
+patents_test = [c["collection"] for c in json.load(open('patents.json'))]
+for c in patents_test:
   collection = GeoJSONTest.FeatureCollectionTest(c)
   collection.test()
+points_test = GeoJSONTest.FeatureCollectionTest(json.load(open('points.json')))
+points_test.test()
